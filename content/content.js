983b45f77e4b546c71f219b10b48b83278875f99
weerut.panjawattana <weerut.panjawattana@thomsonreuters.com>
2016-10-23 00:34:51 +0700

    a

--- content/content.js
+++ content/content.js
@@ -11,11 +11,24 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
             }
             break;
         case "clear-body-style":
-            $("body").removeAttr("style");
-            var childsDiv = $("body>div");
-            for (var i = 0; i < childsDiv.length; i++) {
-                $(childsDiv[i]).removeAttr("style");
+            // $("body").removeAttr("style");
+            // var childsDiv = $("body>div");
+            // for (var i = 0; i < childsDiv.length; i++) {
+            //     $(childsDiv[i]).removeAttr("style");
+            // }
+            var location = window.parent.location.host;
+            if (location.indexOf('pantip') >= 0) {
+                var $head = $("head");
+                var $headlinklast = $head.find("link[rel='stylesheet']:last");
+                var linkElement = 
+                '<link type="text/css" rel="stylesheet" href="/css/version/1473668260/style.css">';
+                if ($headlinklast.length) {
+                    $headlinklast.after(linkElement);
+                } else {
+                    $head.append(linkElement);
+                }
             }
+
             break;
     }
 });
